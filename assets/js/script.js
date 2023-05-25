f31384e4693c46557ebdfc56dd655c0f3da323df
Ijordan4 <124106922+Ijordan4@users.noreply.github.com>
2023-05-25 19:37:39 -0400

    Update script.js #2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
-namespace = "MarvelFlicks".MarvelApi
-const string = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=e3438d4d175fedcdc6c2e3ad5914b6f8&hash=ffd275c5130566a2916217b101f26150"
-var _marvel = new MarvelApi.Marvel();
+const url = 'https://online-movie-database.p.rapidapi.com/auto-complete?q=Avengers';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '78a6737bf0msh512c51f3b45bc1cp117602jsndaa6a14db3f3',
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+	}
+};
 
+const fetchData = async () => {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    const list = data.d;
 
+    list.forEach(item => {
+      const name = item.l;
+      const poster = item.i.imageUrl;
+      const movie = `<li><img src="${poster}"><h2>${name}</h2></li>`;
+      document.querySelector('.movies').innerHTML += movie;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchData();
 
 const menuButton = document.getElementById("menu-button");
 const dropdownMenu = document.querySelector(".absolute.right-0");
@@ -11,3 +33,4 @@ menuButton.addEventListener("click", function () {
   const expanded = menuButton.getAttribute("aria-expanded") === "true" || false;
   menuButton.setAttribute("aria-expanded", !expanded);
   dropdownMenu.style.display = expanded ? "none" : "block";
+});
