87997de26b68dcccc22b530299734e474481ff85
TheWiseSailor <68026214+TheWiseSailor@users.noreply.github.com>
2023-05-26 12:20:02 -0400

    for some reason the api is hinderin with the code but we can figure that out later, I figured out what andrew wanted me to change about the nav bar.... finally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,13 @@
-namespace = "MarvelFlicks".MarvelApi;
-const string =
-  "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=e3438d4d175fedcdc6c2e3ad5914b6f8&hash=ffd275c5130566a2916217b101f26150";
-var _marvel = new MarvelApi.Marvel();
+//namespace = "MarvelFlicks".MarvelApi;
+//const string =
+//  "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=e3438d4d175fedcdc6c2e3ad5914b6f8&hash=ffd275c5130566a2916217b101f26150";
+//var _marvel = new MarvelApi.Marvel();
 
 const menuButton = document.getElementById("menu-button");
-const navbarItems = document.querySelector(".hidden.lg\\:flex");
 
-function toggleMenu() {
+// Add click event listener to the menu button
+menuButton.addEventListener("click", () => {
+  // Toggle the visibility of the navbar items
+  const navbarItems = document.querySelector(".lg\\:flex");
   navbarItems.classList.toggle("hidden");
-}
-
-menuButton.addEventListener("click", toggleMenu);
-
-const clearButton = document.getElementById("clearButton");
-const searchText = document.getElementById("searchText");
-
-clearButton.addEventListener("click", function () {
-  searchText.value = "";
 });
