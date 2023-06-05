4859eea602c6cbd6315e364558e7782b942985c7
Ijordan4 <124106922+Ijordan4@users.noreply.github.com>
2023-06-05 16:57:52 -0400

    Update script.js 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,169 +1,108 @@
-//namespace = "MarvelFlicks".MarvelApi;
-//const string =
-//  "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=e3438d4d175fedcdc6c2e3ad5914b6f8&hash=ffd275c5130566a2916217b101f26150";
-//var _marvel = new MarvelApi.Marvel();
-
 const menuButton = document.getElementById("menu-button");
-const menuItems = document.getElementById("menu-items");
+const dropdownMenu = document.querySelector(".absolute.right-0");
 
-menuButton.addEventListener("click", () => {
-  menuItems.classList.toggle("hidden");
+menuButton.addEventListener("click", function () {
+  const expanded = menuButton.getAttribute("aria-expanded") === "true" || false;
+  menuButton.setAttribute("aria-expanded", !expanded);
+  dropdownMenu.style.display = expanded ? "none" : "block";
 });
 
-const navLinks = menuItems.getElementsByTagName("a");
-for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click", () => {
-    menuItems.classList.add("hidden");
-  });
-}
-//Create Global variable for movie-titles and an array
-var movies = [
-    { name: 'captain-america-fa' },
-    { name: 'captain-marvel' },
-    { name: 'iron-man' },
-    { name: 'iron-Man-2' },
-    { name: 'hulk' },
-    { name: 'thor' },
-    { name: 'the-avengers' },
-    { name: 'thor-dark-world' },
-    { name: 'iron-man-3' },
-    { name: 'captain-america-winter-soldier' },
-    { name: 'guardians' },
-    { name: 'guardians-2' },
-    { name: 'avengers-ultron' },
-    { name: 'ant-man' },
-    { name: 'captain-amaerica-cw' },
-    { name: 'black-widow' },
-    { name: 'spider-man-homecoming' },
-    { name: 'black-panther' },
-    { name: 'doctor-strange' },
-    { name: 'thor-rangarok' },
-    { name: 'antman-wasp' },
-    { name: 'avengers-infinity-war' },
-    { name: 'avengers-end-game' },
-    { name: 'spiderman-far-from-home' },
-    { name: 'shang-chi-ten-rings' },
-    { name: 'eternals' },
-    { name: 'spiderman-no-way-home' },
-    { name: 'doctor-strange-multiverse' },
-    { name: 'thor-love-and-thunder' },
-    { name: 'antman-quantumania' },
-    { name: 'guardians-3' },
- ]
-search.addEventListener("event type", () => {
-    const searchInput = document.querySelector('.input')
-    searchInput.addEventListener("input", (e) => {
-        //declare and assign the value of whater is typed in searchbar
-        let value = e.target.value
-        //Check input and if its greater than 0
-        if (value && value.trim().length > 0) {
-            //Redefine the value, take out white space and change input to all lower-case            
-            value = value.trim().toLowerCase()
-        } else {
-           
-            clearList()
-        }
-    })
-})
-const clearButton = document.getElementById('clear')
-
-clearButton.addEventListener("click", () => {
-    
-})
- 
-// movieList takes in the results of the search
-function movieList(results) {
-    for (const film of results) {
-        //Creates li item for each result
-        const resultItem = document.createElement('li')
-        //adds class to results
-        resultItem.classList.add('result-item')
-        // grabbing the name of the current point of the loop and adding the name as the list item's text
-        const text = document.createTextNode(film.name)
-        //appemds text to result 
-        resultItem.appendChild(text)
-        //appends result to list
-        list.appendChild(resultItem)
-    }
-    if (results.length === 0) {
-        blankForm()
-    }
+// Function to handle movie click event
+function handleMovieClick(link) {
+  window.location.href = link;
 }
 
-searchInput.addEventListener("input", (e) => {
-    let value = e.target.value
-    if (value && value.trim().length > 0) {
-        value = value.trim().toLowerCase()
-
-        //return on the results of the movieList if the search includes movie name
-        movieList(movies.filter(film => {
-            return film.name.includes(value)
-        }))
-    }
-})
-function clearList() {
-    while (list.firstChild) {
-        list.removeChild(list.firstChild)
-    }
-}
-function blankForm() {
-    //create element for error message as list item ("li")
-    const error = document.createElement("li")
-    //add class name of "error-message" to elemeent
-    error.classList.add('error-message')
-    //text that will appear for error message 
-    const text = document.createTextNode('We arent finding what you are searching for!!!')
-    //append text to element we created
-    error.appendChild(text)
-    list.appendChild(error)
-}
-
-
-
-// Build Search bar function
-function fetchMovies(apiKey, callback) {
-  // Construct the API URL
-  const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
-
-  // Make a GET request to the API
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => callback(data.results))
-    .catch(error => console.log(error));
-}
-
-// Function to display movies based on search input
+// Function to handle search
 function searchMovies() {
-  const input = document.getElementById('searchbar').value.toLowerCase();
-  const movies = document.getElementsByClassName('movies');
+  let input = document.getElementById('searchbar').value.toLowerCase();
+  let movies = document.getElementsByClassName('movies');
 
   for (let i = 0; i < movies.length; i++) {
-    const movieTitle = movies[i].innerHTML.toLowerCase();
-    if (!movieTitle.includes(input)) {
-      movies[i].style.display = "none";
+    let movieName = movies[i].getAttribute('data-movie-name').toLowerCase();
+    if (!movieName.includes(input)) {
+      movies[i].style.display = 'none';
     } else {
-      movies[i].style.display = "list-item";
+      movies[i].style.display = 'list-item';
     }
   }
 }
 
-// API key for The Movie Database (TMDB)
+// Array of movie names and corresponding links
+const movies = [
+  { name: 'captain america first avenger', link: 'https://www.themoviedb.org/movie/1771-captain-america-the-first-avenger' },
+  { name: 'captain marvel', link: 'https://www.themoviedb.org/movie/299537-captain-marvel' },
+  { name: 'iron man', link: 'https://www.themoviedb.org/movie/1726-iron-man' },
+  { name: 'iron Man 2', link: 'https://www.themoviedb.org/movie/10138-iron-man-2' },
+  { name: 'the incredible hulk', link: 'https://www.themoviedb.org/movie/1724-the-incredible-hulk' },
+  { name: 'thor', link: 'https://www.themoviedb.org/movie/10195-thor' },
+  { name: 'the avengers', link: 'https://www.themoviedb.org/movie/24428-the-avengers' },
+  { name: 'thor darkworld', link: 'https://www.themoviedb.org/movie/76338-thor-the-dark-world' },
+  { name: 'iron man 3', link: 'https://www.themoviedb.org/movie/68721-iron-man-3' },
+  { name: 'captain america winter soldier', link: 'https://www.themoviedb.org/movie/100402-captain-america-the-winter-soldier' },
+  { name: 'guardians', link: 'https://www.themoviedb.org/movie/118340-guardians-of-the-galaxy' },
+  { name: 'guardians 2', link: 'https://www.themoviedb.org/movie/283995-guardians-of-the-galaxy-2' },
+  { name: 'avengers age of ultron', link: 'https://www.themoviedb.org/movie/99861-avengers-age-of-ultron' },
+  { name: 'ant man', link: 'https://www.themoviedb.org/movie/102899-ant-man' },
+  { name: 'captain amaerica civil war', link: 'https://www.themoviedb.org/movie/271110-captain-america-civil-war' },
+  { name: 'black widow', link: 'https://www.themoviedb.org/movie/497698-black-widow' },
+  { name: 'spider man homecoming', link: 'https://www.themoviedb.org/movie/315635-spider-man-homecoming' },
+  { name: 'black panther', link: 'https://www.themoviedb.org/movie/284054-black-panther' },
+  { name: 'doctor strange', link: 'https://www.themoviedb.org/movie/284052-doctor-strange' },
+  { name: 'thor rangarok', link: 'https://www.themoviedb.org/movie/284053-thor-ragnarok' },
+  { name: 'antman wasp', link: 'https://www.themoviedb.org/movie/363088-ant-man-and-the-wasp' },
+  { name: 'avengers infinity war', link: 'https://www.themoviedb.org/movie/299536-avengers-infinity-war' },
+  { name: 'avengers end game', link: 'https://www.themoviedb.org/movie/299534-avengers-endgame' },
+  { name: 'spiderman far from home', link: 'https://www.themoviedb.org/movie/429617-spider-man-far-from-home' },
+  { name: 'shang-chi ten rings', link: 'https://www.themoviedb.org/movie/566525-shang-chi-and-the-legend-of-the-ten-rings' },
+  { name: 'eternals', link: 'https://www.themoviedb.org/movie/524434-eternals' },
+  { name: 'spiderman no way-home', link: 'https://www.themoviedb.org/movie/634649-spider-man-no-way-home' },
+  { name: 'doctor strange multiverse', link: 'https://www.themoviedb.org/movie/453395-doctor-strange-in-the-multiverse-of-madness' },
+  { name: 'thor love and thunder', link: 'https://www.themoviedb.org/movie/616037-thor-love-and-thunder' },
+  { name: 'antman-quantumania', link: 'https://www.themoviedb.org/movie/640146-ant-man-and-the-wasp-quantumania' },
+  { name: 'guardians 3', link: 'https://www.themoviedb.org/movie/447365-guardians-of-the-galaxy-vol-3' },
+  { name: 'black panther wakanda forever', link: 'https://www.themoviedb.org/movie/505642-black-panther-wakanda-forever' },
+];
 
+function searchMovies(searchText) {
+  const movie = movies.find((m) => m.name === searchText.toLowerCase());
+  if (movie) {
+    window.location.href = movie.link;
+  } else {
+    $("#searchResults").html('<div class="text-white">No results found.</div>');
+  }
+}
 
-//const apiKey = '6fa1d761f4de3a57995383a6c3373f34';
-const apiKey = "6fa1d761f4de3a57995383a6c3373f34"
-// Call fetchMovies function to fetch movie data
-fetchMovies(apiKey, movies => {
-  // Create movie elements
-  const moviesContainer = document.getElementById('movies-container');
-  movies.forEach(movie => {
-    const movieElement = document.createElement('div');
-    movieElement.className = 'movies';
-    movieElement.innerHTML = movie.title;
-    moviesContainer.appendChild(movieElement);
+$(document).ready(function () {
+ 
+
+  $("#searchButton").click(function () {
+    const searchText = $("#searchText").val().toLowerCase();
+    searchMovies(searchText);
   });
 
-  // Add event listener to search bar
-  const searchbar = document.getElementById('searchbar');
-  searchbar.addEventListener('input', searchMovies);
+  
 });
+
+// Perform API request for each movie
+for (const movie of movieNames) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=YOUR_API_KEY&query=${movie.name}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.results.length > 0) {
+        const movieElement = document.createElement('li');
+        movieElement.className = 'movies';
+        movieElement.textContent = data.results[0].title;
+        movieElement.setAttribute('data-movie-name', movie.name);
+        movieElement.style.cursor = 'pointer';
+        movieElement.addEventListener('click', () => {
+          handleMovieClick(movie.link);
+        });
+        document.getElementById('movieList').appendChild(movieElement);
+      }
+    })
+    .catch(error => {
+      console.log('Error occurred while fetching movie data:', error);
+    });
+}
