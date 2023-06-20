547d2164230ae4d09e89cec16e6795e7278992e1
Tomoyuki0788 <131085750+Tomoyuki0788@users.noreply.github.com>
2023-06-19 20:51:43 -0400

    Updated Version??

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,157 @@ for (let i = 0; i < navLinks.length; i++) {
   });
 
 function getHeroData(element,character){
-  const name= 'batman'
-  const url=`https://www.omdbapi.com/?t=${character}&apikey=67a1f106`
+  const name= 'Captain Marvel'
+  const url=`https://www.omdbapi.com/?t=Captain+Marvel&apikey=67a1f106`
 //const url=`https://superheroapi.com/api/10230198626659760/search/${name}`
 
+function getHeroData(element, character){
+  const name= 'Captain America'
+  const url= 'https://www.omdbapi.com/?t=Captain+America&apikey=67a1f106'
+
+function getHeroData(element, character){
+  const name='Iron Man'
+  const url= 'https://www.omdbapi.com/?t=Iron+man&apikey=67a1f106'
+}
+
+function getHeroData(element, character){
+  const name='Iron Man 2'
+  const url='https://www.omdbapi.com/?t=Iron+man+2&apikey=67a1f106'
+}
+
+function getHeroData(element, character){
+  const name= 'The Incredible Hulk'
+  const url='https://www.omdbapi.com/?t=The+Incredible+Hulk&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Thor'
+  const url='https://www.omdbapi.com/?t=Thor&apikey=67a1f106'
+}
+
+function getHeroData(element, character){
+  const name='The Avengers'
+  const url='https://www.omdbapi.com/?t=The+Avengers&apikey=67a1f106'
+}
+function getHeroData(element, character){
+  const name='Thor:The Dark World'
+  const url='https://www.omdbapi.com/?t=Thor+The+dark+World&apikey=67a1f106'
+}
+function getHeroData(element,character){
+  const name='Iron Man 3'
+  const url='https://www.omdbapi.com/?t=Iron+Man+3&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Captain America The Winter Soldier'
+  const url='https://www.omdbapi.com/?t=Captain+America+The+Winter+Soldier&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Guardians of the Galaxy'
+  const url='https://www.omdbapi.com/?t=Guardians+of+the+galaxy&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Guardians of the Galaxy 2'
+  const url='https://www.omdbapi.com/?t=Guardians+of+the+galaxy=2&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Avengers Age of Ultron'
+  const url='https://www.omdbapi.com/?t=Avengers+Age+of+Ultron&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Ant-Man'
+  const url='https://www.omdbapi.com/?t=Ant+Man&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Captain America:Civil War'
+  const url='https://www.omdbapi.com/?t=Captain+America+Civil+war&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='BLack Widow'
+  const url='https://www.omdbapi.com/?t=Black+Widow&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Spider man: Homecoming'
+  const url='https://www.omdbapi.com/?t=Spider+Man+Homecoming&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Black Panther'
+  const url='https://www.omdbapi.com/?t=Black+Panther&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Doctor Strange'
+  const url='https://www.omdbapi.com/?t=Doctor+Strange&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Thor Ragnarok'
+  const url='https://www.omdbapi.com/?t=Thor+Ragnarok&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Ant-Man and the wasp'
+  const url='https://www.omdbapi.com/?t=Ant+Man+and+the+wasp&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Avengers: Infinity War'
+  const url='https://www.omdbapi.com/?t=Avengers+Infinity+war&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Avengers: End Game'
+  const url='https://www.omdbapi.com/?t=Avengers+End+Game&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Spider Man: Far From Home'
+  const url='https://www.omdbapi.com/?t=Spider+Man+Far+from+Home&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name= 'Shang Chi and The Legend of the Ten Rings'
+  const url='https://www.omdbapi.com/?t=Shang+Chi+and+the=legend+of+the+Ten+Rings&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Eternals'
+  const url='https://www.omdbapi.com/?t=Eternals&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Spider Man: No Way Home'
+  const url='https://www.omdbapi.com/?t=Spider+man+no+way+home&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Doctor Strange in the Multiverse of Madness'
+  const url='https://www.omdbapi.com/?t=Doctor+Strange+in+the+Multiverse+of+Madness&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Thor: Love and Thunder'
+  const url='https://www.omdbapi.com/?t=Thor+Love+and+Thunder&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Ant Man and the Wasp: Quantumania'
+  const url='https://www.omdbapi.com/?t=Ant+Man+and+the+wasp+Quantumania&apikey=67a1f106'
+}
+
+function getHeroData(element,character){
+  const name='Guardians of the Galaxy Vol 3'
+  const url='https://www.omdbapi.com/?t=Guardians+of+the+Galaxy+Vol+3&apikey=67a1f106'
+}
+}
 fetch(url)
 
 .then(response=>response.json())
